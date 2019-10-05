ea4356f5b405d3fd044b95499980ecff8fd204d7
tdragon00 <47230462+tdragon00@users.noreply.github.com>
2019-10-05 15:34:56 -0700

    added key.h

---
+++ iCyberSecurity-master/iCyberSecurity-master/iCyberSecurity/Key.h
@@ -0,0 +1,13 @@
+#include<string.h>
+#ifndef KEY_H
+#define KEY_H
+
+
+
+enum Key
+{nice,key,notnice
+
+};
+
+static string Keyoutputs[]
+#endif // KEY_H
--- iCyberSecurity-master/iCyberSecurity-master/iCyberSecurity/iCyberSecurity.pro
+++ iCyberSecurity-master/iCyberSecurity-master/iCyberSecurity/iCyberSecurity.pro
@@ -20,6 +20,7 @@ SOURCES += \
     mainwindow.cpp
 
 HEADERS += \
+    Key.h \
     mainwindow.h
 
 FORMS += \
